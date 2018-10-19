fad179435457b6c161897b1439eb79d12fb42fc6
Clement Verna <cverna@tutanota.com>
2018-10-19 14:58:30 +0200

    Add subpkgs relation to the Package Model
    Signed-off-by: Clement Verna <cverna@tutanota.com>

--- fedsearch/pkgsearch/models.py
+++ fedsearch/pkgsearch/models.py
@@ -8,3 +8,11 @@ class Package(models.Model):
     point_of_contact = models.CharField(max_length=100)
     icon = models.CharField(max_length=100)
     upstream_url = models.CharField(max_length=100)
+
+
+class SubPackage(models.Model):
+    name = models.CharField(max_length=100)
+    summary = models.TextField()
+    description = models.TextField()
+    icon = models.CharField(max_length=100)
+    package = models.ForeignKey(Package, related_name="subpkgs", on_delete=models.CASCADE)
--- fedsearch/pkgsearch/tests/test_models.py
+++ fedsearch/pkgsearch/tests/test_models.py
@@ -3,21 +3,48 @@ from django.test import TestCase
 from mixer.backend.django import mixer
 
 
-from fedsearch.pkgsearch.models import Package
+from fedsearch.pkgsearch.models import Package, SubPackage
 
 pytestmark = pytest.mark.django_db
 
 
 class PackageTests(TestCase):
     def test_create(self):
+        """Ensure we can create an instance of Package."""
         mixer.blend(Package)
         assert Package.objects.count() == 1
 
     def test_query(self):
+        """Ensure we can query an instance of Package."""
         mixer.blend(
             Package,
             name="vim",
             summary="The VIM version of the vi editor for the X Window System - GVim",
         )
         r = Package.objects.filter(summary__contains="editor")
-        assert r.values()[0]["name"] == "vim"
+        for pkg in r.all():
+            assert pkg.name == "vim"
+
+    def test_query_subpkg(self):
+        """Ensure we can query an instance of Package and get subpackages info"""
+        pkg = mixer.blend(Package, name="gcc")
+        mixer.blend(SubPackage, name="cpp", package=pkg)
+        r = Package.objects.filter(name__contains="gcc")
+        for pkg in r.all():
+            assert pkg.name == "gcc"
+            for subpkg in pkg.subpkgs.all():
+                assert subpkg.name == "cpp"
+
+
+class SubPackageTests(TestCase):
+    def test_create(self):
+        mixer.blend(SubPackage)
+        assert SubPackage.objects.count() == 1
+
+    def test_query(self):
+        pkg = mixer.blend(Package, name="gcc")
+        mixer.blend(SubPackage, name="cpp", package=pkg)
+        r = SubPackage.objects.filter(name__contains="cpp")
+        for subpkg in r.all():
+            assert subpkg.name == "cpp"
+            assert subpkg.package.name == "gcc"

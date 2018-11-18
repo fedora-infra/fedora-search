4d5e22f1071d03aae594436964fea6ae3816e477
Clement Verna <cverna@tutanota.com>
2018-11-18 18:28:13 +0100

    Configure DRF
    Signed-off-by: Clement Verna <cverna@tutanota.com>

--- fedsearch/settings/base.py
+++ fedsearch/settings/base.py
@@ -69,6 +69,13 @@ TEMPLATES = [
     }
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}
+
 WSGI_APPLICATION = "fedsearch.wsgi.application"
 
 

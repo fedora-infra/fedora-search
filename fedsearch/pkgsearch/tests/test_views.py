2c759156bd8d38bd7152d47d27535797899438c2
Clement Verna <cverna@tutanota.com>
2018-11-19 09:20:44 +0100

    Add pagination to the PackageSearchView
    Signed-off-by: Clement Verna <cverna@tutanota.com>

--- fedsearch/pkgsearch/tests/test_views.py
+++ fedsearch/pkgsearch/tests/test_views.py
@@ -1,9 +1,7 @@
-from mixer.backend.django import mixer
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
-from fedsearch.pkgsearch.models import Package
 from fedsearch.pkgsearch.tests.utils import create_pkgs
 
 
@@ -16,13 +14,13 @@ class PackageViewTests(APITestCase):
         url = reverse("search")
         response = self.client.get(url, {"q": "gnome-terminal"})
         assert response.status_code == status.HTTP_200_OK
-        assert response.data[0]["name"] == "gnome-terminal"
+        assert response.data["results"][0]["name"] == "gnome-terminal"
 
     def test_search_package_summary(self):
         url = reverse("search")
         response = self.client.get(url, {"q": "terminal"})
         assert response.status_code == status.HTTP_200_OK
-        assert len(response.data) == 3
-        assert response.data[0]["name"] == "mate-terminal"
-        assert response.data[1]["name"] == "gnome-terminal"
-        assert response.data[2]["name"] == "tmux"
+        assert len(response.data["results"]) == 3
+        assert response.data["results"][0]["name"] == "mate-terminal"
+        assert response.data["results"][1]["name"] == "gnome-terminal"
+        assert response.data["results"][2]["name"] == "tmux"
--- fedsearch/pkgsearch/views.py
+++ fedsearch/pkgsearch/views.py
@@ -1,5 +1,5 @@
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.settings import api_settings
 
 from fedsearch.pkgsearch.models import Package
 from fedsearch.pkgsearch.serializers import PackageSerializer
@@ -7,10 +7,18 @@ from fedsearch.pkgsearch.serializers import PackageSerializer
 
 class PackageSearchView(APIView):
     def get(self, request):
+        pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+        paginator = pagination_class()
+
         query = request.query_params.get("q")
         queryset = Package.objects.search(query)
-        results = []
-        for pkg in queryset:
-            serializer = PackageSerializer(pkg)
-            results.append(serializer.data)
-        return Response(results)
+
+        page = paginator.paginate_queryset(queryset, request)
+        if page is not None:
+            serializer = PackageSerializer(page, many=True)
+            results = paginator.get_paginated_response(serializer.data)
+        else:
+            serializer = PackageSerializer(queryset, many=True)
+            results = serializer.data
+
+        return results
